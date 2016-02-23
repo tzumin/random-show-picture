54dc01a31b2c688fe569a450bed7ec8193637d16
tzumin <lizlin@accuvally.com>
2016-02-23 17:53:17 +0800

    add random img position method
    - add randomNumber method to create unuse index
    - add setImgOrder method to preload img and set image position

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@ class RandomImgComponent {
     this.viewHeight = window.innerHeight;
     this.baseSize = this.viewWidth * 0.75 > this.viewHeight ? Math.floor(this.viewHeight / 3) : Math.floor(this.viewWidth / 4);
     this.imgSum = 12;
+    this.imgAssignList = this.setImgOrder();
     this.init();
   }
   init() {
@@ -17,6 +18,28 @@ class RandomImgComponent {
     boxElement.style.margin = "auto";
     this.boxElement = boxElement;
     document.body.appendChild(boxElement);
+  }
+  setImgOrder() {
+    const imageList = {};
+    const imgAssignFlagObj = {};
+    for(let i = this.imgSum - 1; i >= 0 ; i--) {
+      const index = this.randomNumber(imgAssignFlagObj);
+      const newImg = this.imgElement.cloneNode();
+      newImg.src = 'images/' + i + '.png';
+      imageList[i] = {
+        element: newImg,
+        positionIndex: index
+      };
+    }
+    return imageList;
+  }
+  randomNumber(imgAssignFlagObj) {
+    let randomNum = Math.floor(Math.random() * 12);
+    while(imgAssignFlagObj.hasOwnProperty(randomNum)) {
+      randomNum = Math.floor(Math.random() * 12);
+    }
+    imgAssignFlagObj[randomNum] = true;
+    return randomNum;
   }
 }
 
